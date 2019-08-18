a0d84d3adcaf84214ff0649e7c3bfcda7e87c128
supaksi <52766675+supaksi@users.noreply.github.com>
2019-08-18 16:45:59 +0900

    preworks4 update

---
+++ waterDropBeta2_preworks4/Assets/Scripts/Valve.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Valve : MonoBehaviour
+{
+    public GameObject waterPrefab;
+
+    private Vector3 m_Position;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_Position = transform.position;
+        Instantiate(waterPrefab, m_Position, Quaternion.identity);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ waterDropBeta2_preworks4/Valve 에서 물방울이 떨어지도록 제작.md
@@ -0,0 +1,18 @@
+Valve 에서 물방울이 떨어지도록 제작
+
+- Valve의 갯수 설정
+
+  - loop를 활용해서
+
+    - while loop	
+    - for loop
+
+    
+
+- 물방울은 Prefab으로 셋팅 하여 Valve에서 컨트롤 하도록
+
+  
+
+
+
+Valve 
